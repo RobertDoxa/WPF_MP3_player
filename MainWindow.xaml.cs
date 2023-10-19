5193792c6385c183f028c28b76d567569b574d86
RobertDoxa <robert.doksa@gmail.com>
2023-10-19 07:24:08 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ControlzEx.Theming;
+using MahApps.Metro.Controls;
+using Microsoft.Win32;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem item = sender as MenuItem;
+            this.Title = "File: " + item.Header;
+        }
+
+        private void MenuItem_Click1(object sender, RoutedEventArgs e)
+        {
+            MenuItem item = sender as MenuItem;
+            this.Title = "Edit: " + item.Header;
+        }
+
+        private void MenuItem_Click2(object sender, RoutedEventArgs e)
+        {
+            MenuItem item = sender as MenuItem;
+            this.Title = "View: " + item.Header;
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 files (*.mp3)|WAV files (*.wav)";
+            if (openFileDialog.ShowDialog() == true)
+                //string filename = dlg.FileName;
+                //textBox1.Text = filename;
+        }
+
+        private void LaunchGitHubSite(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new ProcessStartInfo
+            {
+                FileName = "https://github.com/RobertDoxa",
+                UseShellExecute = true
+            });
+        }
+        private void DarkModeClick(object sender, RoutedEventArgs e)
+        {
+            ThemeManager.Current.ChangeTheme(this, "Dark.Orange");
+        }
+        private void LightModeClick(object sender, RoutedEventArgs e)
+        {
+            ThemeManager.Current.ChangeTheme(this, "Light.Orange");
+        }
+    }
+}
